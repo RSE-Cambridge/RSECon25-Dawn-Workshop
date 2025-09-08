a3a48ee95b0a47f50133e2012aefabbbe37bd418
K. Harrison <kh296@users.noreply.github.com>
2025-09-08 17:40:17 +0100

    Added link to miniforge3 directory

--- scripts/workshop_setup.sh
+++ scripts/workshop_setup.sh
@@ -6,6 +6,9 @@ WORKSHOP_RDS="/home/kh296/rds/rds-rsecon/kh296"
 mkdir -p ../install
 cp ${WORKSHOP_RDS}/install/*setup.sh ../install
 
+rm -rf ~/miniforge3
+ln -s ${WORKSHOP_RDS}/miniforge3 ~/miniforge3
+
 JUPYTER_KERNELS_HOME="${HOME}/.local/share/jupyter/kernels"
 JUPYTER_KERNELS_RDS="${WORKSHOP_RDS}/jupyter/kernels"
 cp -rp ${JUPYTER_KERNELS_RDS}/* ${JUPYTER_KERNELS_HOME}
