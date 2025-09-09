61f638a6b93c5d0e019a436f0c1749c4c84693ab
K. Harrison <kh296@users.noreply.github.com>
2025-09-09 08:12:03 +0100

    Updated environment setup

--- scripts/workshop_setup.sh
+++ scripts/workshop_setup.sh
@@ -1,8 +1,9 @@
 #/bin/bash
 # Script for copying workshop setup scripts and kernels.
+WORKSHOP_HOME=$(pwd)
 WORKSHOP_RDS="${HOME}/rds/rds-rsecon/kh296"
 
-# Copyt setup scripts.
+# Copy setup scripts.
 mkdir -p ../install
 cp ${WORKSHOP_RDS}/install/*setup.sh ../install
 
@@ -17,3 +18,13 @@ cp -r ${JUPYTER_KERNELS_RDS}/* ${JUPYTER_KERNELS_HOME}
 for VENV in $(ls ${JUPYTER_KERNELS_HOME}); do
     sed -i "s@/.*/rds/@${HOME}/rds/@g" ${JUPYTER_KERNELS_HOME}/${VENV}/kernel.json
 done
+
+# Clone repository with examples.
+cd $"WORKSHOP_HOME"
+if [ ! -d practical-ml-with-pytorch ]; then
+  git clone https://github.com/kh296/practical-ml-with-pytorch
+  cd practical-ml-with-pytorch
+  git checkout xpu
+  cd 
+fi
+cd $"WORKSHOP_HOME"
